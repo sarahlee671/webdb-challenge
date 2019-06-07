1ab8ff9f4367d092b7c90001c823246a5ebc365f
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-06-07 08:35:48 -0700

    added knexfile with development information

---
+++ knexfile.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/projects.db3', // the folder will be created when we run the migrations
+    },
+    // moving migrations to the data folder
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  }
+}
